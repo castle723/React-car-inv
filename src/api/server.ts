c649fda1a5c6622e840b4c3c294fa88b5a9bb512
castle723 <jacob@qgmnow.com>
2023-11-29 01:41:58 -0800

    first commit

---
+++ src/api/server.ts
@@ -0,0 +1,81 @@
+const token = '5db5c38809c2d424e08ea5ae0d6817fd9d521dd48c5d5f1f'
+
+export const server_calls = {
+    get: async () => { 
+        const response = await fetch(`http://127.0.0.1:5000/api/cars`,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            }
+
+        });
+
+        if (!response.ok){
+            throw new Error('Failed to fetch data from the server')
+        }
+
+        return await response.json()
+    },
+
+    create: async (data: any = {}) => {
+        const response = await fetch(`http://127.0.0.1:5000/api/cars`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to create new data on the server')
+        }
+
+        return await response.json()
+    },
+
+    update: async (id: string, data:any = {}) => {
+        const response = await fetch(`http://127.0.0.1:5000/api/car${id}`,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to update data on the server')
+        }
+
+        return await response.json()
+    },
+
+    delete: async (id: string) => {
+        const response = await fetch(`http://127.0.0.1:5000/api/car${id}`,
+        {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            },
+
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to delete data from the server')
+        }
+
+        return;
+    },
+}
