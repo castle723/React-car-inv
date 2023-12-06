1dd67489d321e98d8a70c0686c86bd0eca7efb22
castle723 <jacob@qgmnow.com>
2023-12-06 14:06:48 -0800

    final commit

--- src/config/auth0.config.ts
+++ src/config/auth0.config.ts
@@ -1,4 +1,4 @@
 export const auth0Config = {
-    domain: "dev-0ie2in1ky4enps46.us.auth0.com",
-    clientId: "boIqcgOIRpMrdwLDQWohwIAXkj5d6F4j",
-  };
+  domain: "dev-0ie2in1ky4enps46.us.auth0.com",
+  clientId: "dUGg9mXZGEQliPXqORnqqf9G63JHJcJp",
+};
